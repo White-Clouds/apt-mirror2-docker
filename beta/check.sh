1920e5e4a16a28606e062ad7e3ce413cd3fc7545
White-Clouds <36442882+White-Clouds@users.noreply.github.com>
2024-05-16 00:25:39 +0800

    Update beta

---
+++ beta/check.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+base_path=$(grep "^set base_path" /etc/apt/mirror.list | cut -d ' ' -f 3)
+if [ -z "$base_path" ]; then
+    echo "Error: Unable to find base_path in /etc/apt/mirror.list"
+    exit 1
+fi
+log_path="${base_path}/var/*.log"
+for file in $log_path; do
+    if [ $(tail -n 40 "$file" | grep -c "Metadata moved") -eq 1 ]; then
+        break
+    fi
+    if [ $(tail -n 40 $file | grep -c "0.0 B/sec") -eq 40 ]
+    then
+        pkill -9 apt-mirror
+        current_hour=$(date +%H)
+        current_minute=$(date +%M)
+        if echo $CRON_SCHEDULE | awk '{print $2}' | grep -q $(($current_hour + ($current_minute >= 30 ? 1 : 0)))
+        then
+            break
+        else
+            apt-mirror > /proc/1/fd/1 2>/proc/1/fd/2
+            break
+        fi
+    fi
+done
---
+++ beta/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cp /usr/share/zoneinfo/$TZ /etc/localtime
+echo $TZ > /etc/timezone
+echo "TZ=$TZ" > /etc/crontabs/apt-mirror2
+echo "PATH=$PATH" >> /etc/crontabs/apt-mirror2
+echo "$CRON_SCHEDULE /bin/sh -c 'apt-mirror > /proc/1/fd/1 2>/proc/1/fd/2'" >> /etc/crontabs/apt-mirror2
+echo "*/20 * * * * /bin/sh -c /check.sh" >> /etc/crontabs/apt-mirror2
+chmod 0644 /etc/crontabs/apt-mirror2
+crontab /etc/crontabs/apt-mirror2
+crond -f
