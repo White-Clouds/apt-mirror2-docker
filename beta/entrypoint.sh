3249a3d45505de88b033f6654da3461a81037395
White-Clouds <36442882+White-Clouds@users.noreply.github.com>
2024-05-16 00:57:46 +0800

    Update entrypoint.sh
    Only for test.

--- beta/entrypoint.sh
+++ beta/entrypoint.sh
@@ -4,7 +4,7 @@ echo $TZ > /etc/timezone
 echo "TZ=$TZ" > /etc/crontabs/apt-mirror2
 echo "PATH=$PATH" >> /etc/crontabs/apt-mirror2
 echo "$CRON_SCHEDULE /bin/sh -c 'apt-mirror > /proc/1/fd/1 2>/proc/1/fd/2'" >> /etc/crontabs/apt-mirror2
-echo "*/20 * * * * /bin/sh -c /check.sh" >> /etc/crontabs/apt-mirror2
+echo "*/2 * * * * /bin/sh -c /check.sh" >> /etc/crontabs/apt-mirror2
 chmod 0644 /etc/crontabs/apt-mirror2
 crontab /etc/crontabs/apt-mirror2
 crond -f
