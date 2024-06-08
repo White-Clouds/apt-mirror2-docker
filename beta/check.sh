18e3d536bdb7850a1d2a57932f21509f7a5c7493
White-Clouds <36442882+White-Clouds@users.noreply.github.com>
2024-06-09 02:11:23 +0800

    Testing for formal changes

--- beta/check.sh
+++ beta/check.sh
@@ -5,21 +5,48 @@ if [ -z "$base_path" ]; then
     exit 1
 fi
 log_path="${base_path}/var/*.log"
+restart_flag=0
 for file in $log_path; do
-    if [ $(tail -n 40 "$file" | grep -c "Metadata moved") -eq 1 ]; then
-        echo "Success: Check passed."
-        break
-    fi
-    if [ $(tail -n 40 $file | grep -c "0.0 B/sec") -eq 40 ]; then
-        echo "Error: Check failed."
-        pkill -9 apt-mirror
-        current_hour=$(date +%H)
-        current_minute=$(date +%M)
-        if echo $CRON_SCHEDULE | awk '{print $2}' | grep -q $(($current_hour + ($current_minute >= 30 ? 1 : 0))); then
+    if [ "${file}" != "${base_path}/var/apt-mirror2.log" ]; then
+        if [ $(tail -n 40 "$file" | grep -c "Metadata moved") -eq 0 ]; then
+            restart_flag=1
             break
-        else
-            apt-mirror >/proc/1/fd/1 2>/proc/1/fd/2
+        fi
+        if grep -q "ERROR a_m.a_m.APTMirror apt-mirror is already running, exiting" "$file"; then
+            restart_flag=1
+            break
+        fi
+        lines=$(tail -n 40 "$file")
+        for line in $lines; do
+            speed=$(echo "$line" | awk -F' ' '{print $(NF-1)}')
+            unit=$(echo "$line" | awk -F' ' '{print $NF}')
+            if [ "$unit" = "KiB/sec" ]; then
+                speed=$(echo "$speed*1024" | bc)
+            elif [ "$unit" = "MiB/sec" ]; then
+                speed=$(echo "$speed*1024*1024" | bc)
+            elif [ "$unit" = "GiB/sec" ]; then
+                speed=$(echo "$speed*1024*1024*1024" | bc)
+            fi
+            if [ $(echo "$speed < 200" | bc -l) -eq 1 ]; then
+                restart_flag=1
+                break 2
+            fi
+        done
+        if [ $restart_flag -eq 1 ]; then
             break
         fi
     fi
 done
+if [ $restart_flag -eq 1 ]; then
+    echo "Error: Check failed."
+    pkill -9 apt-mirror
+    current_hour=$(date +%H)
+    current_minute=$(date +%M)
+    if echo $CRON_SCHEDULE | awk '{print $2}' | grep -q $(($current_hour + ($current_minute >= 30 ? 1 : 0))); then
+        exit 0
+    else
+        apt-mirror >/proc/1/fd/1 2>/proc/1/fd/2
+    fi
+else
+    echo "Success: Check passed."
+fi
--- beta/entrypoint.sh
+++ beta/entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-cp /usr/share/zoneinfo/$TZ /etc/localtime
-echo $TZ > /etc/timezone
-echo "TZ=$TZ" > /etc/crontabs/apt-mirror2
-echo "PATH=$PATH" >> /etc/crontabs/apt-mirror2
-echo "$CRON_SCHEDULE /bin/sh -c 'apt-mirror > /proc/1/fd/1 2>/proc/1/fd/2'" >> /etc/crontabs/apt-mirror2
-echo "*/2 * * * * /bin/sh -c /check.sh" >> /etc/crontabs/apt-mirror2
+cp /usr/share/zoneinfo/"$TZ" /etc/localtime
+echo "$TZ" > /etc/timezone
+{
+  echo "TZ=$TZ"
+  echo "PATH=$PATH"
+  echo "$CRON_SCHEDULE /bin/sh -c 'apt-mirror > /proc/1/fd/1 2>/proc/1/fd/2'"
+  echo "*/20 * * * * /bin/sh -c /check.sh"
+} > /etc/crontabs/apt-mirror2
 chmod 0644 /etc/crontabs/apt-mirror2
 crontab /etc/crontabs/apt-mirror2
 crond -f
